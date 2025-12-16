d07c7bbb6dd5783a0569f101db4ac6ddcff66c59
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-12-16 12:01:45 +0100

    [add] feat end milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
-import { useState, useEffect, useMemo } from "react"
+import React, { useState, useEffect, useMemo } from "react"
+
+function PoliticianCard({ name, position, image, biography }) {
+  console.log('card')
+  return (
+    <li className="card">
+      <h2 className="pt-20">{name} ({position})</h2>
+      <div className="d-flex">
+        <img src={image} alt={name} />
+        <p className="mx-10">{biography}</p>
+      </div>
+    </li>
+  )
+}
+
+const MemoizedPoliticianCard = React.memo(PoliticianCard)
 
 function App() {
 
@@ -28,15 +43,7 @@ function App() {
         onChange={e => setText(e.target.value)} />
       <ul>
         {filteredPoliticians.map(politician => {
-          return (
-            <li className="card" key={politician.id}>
-              <h2 className="pt-20">{politician.name} ({politician.position})</h2>
-              <div className="d-flex">
-                <img src={politician.image} alt={politician.name} />
-                <p className="mx-10">{politician.biography}</p>
-              </div>
-            </li>
-          )
+          return <MemoizedPoliticianCard key={politician.id} {...politician} />
         })}
       </ul>
     </div>
