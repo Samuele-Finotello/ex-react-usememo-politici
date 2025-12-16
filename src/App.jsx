2fd181a51b4af22b9aed4802ce0aeb51cb073067
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-12-16 11:28:00 +0100

    [add] feat end milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 
 function App() {
 
   const [politicians, setPoliticians] = useState([])
+  const [text, setText] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:3333/politicians')
@@ -11,19 +12,28 @@ function App() {
       .catch(error => console.error(error))
   }, [])
 
-  console.log(politicians)
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(politician => {
+      const isInFilter = politician.name.toLowerCase().includes(text.toLowerCase()) || politician.biography.toLowerCase().includes(text.toLowerCase())
+      return isInFilter;
+    })
+  }, [politicians, text])
 
   return (
     <div>
       <h1>Lista di politici</h1>
+      <input type="text"
+        placeholder="Inserisci ricerca"
+        value={text}
+        onChange={e => setText(e.target.value)} />
       <ul>
-        {politicians.map(politician => {
+        {filteredPoliticians.map(politician => {
           return (
             <li className="card" key={politician.id}>
               <h2 className="pt-20">{politician.name} ({politician.position})</h2>
               <div className="d-flex">
                 <img src={politician.image} alt={politician.name} />
-                <p className="ms-10">{politician.biography}</p>
+                <p className="mx-10">{politician.biography}</p>
               </div>
             </li>
           )
